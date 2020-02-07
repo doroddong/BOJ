88379e2bf5f730c09ac626d747a8d4fee32090da
doroddong <dongpang1014@ajou.ac.kr>
2020-02-07 23:40:49 +0900

    #2231

---
+++ 2231_분해합.cpp
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+int main() {
+	long n;
+	cin >> n;
+	long m = n;
+	int digit = 0;
+	bool find = false;
+
+	while (m) {
+		m /= 10;
+		digit++;
+	}
+	long begin = n - digit * 9;
+	char str[10];
+	long sum = 0;
+	
+	for (long i = begin; i <= n; i++) {
+		sprintf(str, "%ld", i);
+		sum = i;
+		for (int j = 0; j < strlen(str); j++)
+			sum += str[j] - '0';
+		if (sum == n) {
+			cout << i << '\n';
+			find = true;
+			break;
+		}
+	}
+	if (find == false)
+		cout << "0\n";
+	return 0;
+}
