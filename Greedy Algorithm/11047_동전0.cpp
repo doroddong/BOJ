085508c2f56bb40d871b5e46ea461ccd9e7b522d
doroddong <dongpang1014@ajou.ac.kr>
2020-03-09 22:01:03 +0900

    BOJ

---
+++ Greedy Algorithm/11047_동전0.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	int N, K;
+	int coin = 0;
+	int cost[11];
+
+	cin >> N >> K;
+
+	for (int i = 0; i < N; i++)
+		cin >> cost[i];
+
+	for (int i = N - 1; i >= 0; i--) {
+		if (K == 0)
+			break;
+		while(K>=cost[i]){
+				K -= cost[i];
+				coin++;
+		}
+	}
+	cout << coin << "\n";
+	return 0;
+}
